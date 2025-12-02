64ed36fff03da91c14b1d0e352282b07a4688716
OU Aymeric <cookiedonut92@gmail.com>
2025-12-02 15:51:22 +0100

    up chap 1

---
+++ chapters/chapter_1.py
@@ -0,0 +1,6 @@
+###introduction to chapter 1
+
+print("Welcome to Chapter 1: The Beginning of Your Adventure!")
+print("In this chapter, you will embark on a journey filled with challenges and discoveries.")
+print("Welcome to the world of Harry Potter!")
+
