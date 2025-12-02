1a73c0205a26a452720b1130711a22d30d43f8dc
OU Aymeric <cookiedonut92@gmail.com>
2025-12-02 16:40:28 +0100

    patch indent

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -12,51 +12,51 @@ def ask_text(message: str):
     """Demande à l'utilisateur de saisir du texte non vide."""
 
 
-def ask_number(message: str, min_val=None, max_val=None):
-while True:
-    if min_val is not None and max_val is not None:
-        user_input = input(message , f"({min_val} - {max_val})").strip()
-        try:
-            value = int(user_input)
-            if (min_val is not None and value < min_val) or (max_val is not None and value > max_val):
-                if min_val is not None and max_val is not None:
-                    print(f"Please enter a number between {min_val} and {max_val}.")
-                elif min_val is not None:
+def ask_number(message : str, min_val : int = None, max_val: int = None):
+    while True:
+        if min_val is not None and max_val is not None:
+            user_input = input(message , f"({min_val} - {max_val})").strip()
+            try:
+                value = int(user_input)
+                if (min_val is not None and value < min_val) or (max_val is not None and value > max_val):
+                    if min_val is not None and max_val is not None:
+                        print(f"Please enter a number between {min_val} and {max_val}.")
+                    elif min_val is not None:
+                        print(f"Please enter a number greater than or equal to {min_val}.")
+                    elif max_val is not None:
+                        print(f"Please enter a number less than or equal to {max_val}.")
+                else:
+                    return value
+
+        elif min_val is not None and max_val is None:
+            user_input = input(message , f"(minimum {min_val})").strip()
+            try:
+                value = int(user_input)
+                if value < min_val:
                     print(f"Please enter a number greater than or equal to {min_val}.")
-                elif max_val is not None:
+                else:
+                    return value
+
+        elif max_val is not None and min_val is None:
+            user_input = input(message , f"(maximum {max_val})").strip()
+            try:
+                value = int(user_input)
+                if value > max_val:
                     print(f"Please enter a number less than or equal to {max_val}.")
-            else:
+                else:
+                    return value
+
+        else:
+            user_input = input(message).strip()
+            try:
+                value = int(user_input)
                 return value
 
-    elif min_val is not None and max_val is None:
-        user_input = input(message , f"(minimum {min_val})").strip()
-        try:
-            value = int(user_input)
-            if value < min_val:
-                print(f"Please enter a number greater than or equal to {min_val}.")
-            else:
-                return value
 
-    elif max_val is not None and min_val is None:
-        user_input = input(message , f"(maximum {max_val})").strip()
-        try:
-            value = int(user_input)
-            if value > max_val:
-                print(f"Please enter a number less than or equal to {max_val}.")
-            else:
-                return value
-
-    else:
-        user_input = input(message).strip()
-        try:
-            value = int(user_input)
-            return value
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
 
 
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-
-
-
+def ask_choice(message, options)
 
 
